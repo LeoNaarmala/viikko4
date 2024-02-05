c9e0390b2d13c93c06cfe08d4590e76136783a23
Leo <leo.naarmala@student.lut.fi>
2024-02-05 21:41:04 +0200

    modified:   src/main/java/main/Student.java 	modified:   target/classes/main/Student.class

--- src/main/java/main/Student.java
+++ src/main/java/main/Student.java
@@ -1,15 +1,16 @@
 package main;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 
 public class Student {
-    HashMap<String, String> students = new HashMap<String, String>();
+    HashMap<String, String> students = new LinkedHashMap<String, String>();
     public void addStudent(String name, String snum) {
-        students.put(snum,name);
+        students.put(name,snum);
     }
     public void listStudent(){
         System.out.println("Opiskelijat:");
         for (String i : students.keySet()) {
-            System.out.println( i + ": " + students.get(i));}
+            System.out.println( students.get(i) + ": " +i );}
     }
 }
 
